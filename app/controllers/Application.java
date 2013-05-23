69d9c9275999421008a2d470ef3b6d56a6f6553b
axel <axel.olsson@aklagare.se>
2013-05-23 21:17:36 +0200

    cobra

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -16,7 +16,7 @@ public class Application extends Controller {
     	katten.id = 1L;
     	katten.comment = "Så att...";
     	katten.name = "Tiger";
-    	katten.pictureUrl = "http://images4.fanpop.com/image/photos/16100000/-cats-16140154-1920-1080.jpg";
+    	katten.pictureUrl = "http://25.media.tumblr.com/fbc58eea389fd367db023e0063752652/tumblr_mn4b9zzxuk1r5ub99o1_500.jpg";
     	katten.score = 10;
     	
 
@@ -25,7 +25,7 @@ public class Application extends Controller {
     	katten1.id = 2L;
     	katten1.comment = "Äckel gillar att gå på ditt ansikte när du sover, älskar att bajsa i soffan för att så att du ska känna dig överraskad när du sätter dig där. Samt är en jävle på att putta ner telefoner från bord osv....";
     	katten1.name = "Äckel";
-    	katten1.pictureUrl = "http://legacy-cdn.smosh.com/smosh-pit/122010/ugly-cat-9.jpg";
+    	katten1.pictureUrl = "http://www.tntmagazine.com/media/content/_master/39266/images/cat-bread-5.jpg";
     	katten1.score = -20;
     	
     	return ok(Json.toJson(Arrays.asList(katten, katten1)));
