444750bb3fcc1e450e09620a6e1261c3b85fe2cf
axel <axel.olsson@aklagare.se>
2013-05-27 15:38:51 +0200

    meza

--- public/javascripts/cats.js
+++ public/javascripts/cats.js
@@ -1,30 +1,33 @@
 var app = angular.module('cats', ['ui.state', '$strap'])
   .config(function($stateProvider, $urlRouterProvider) {
-
     $urlRouterProvider
       .otherwise('/cats');
 
     $stateProvider
       .state('cats', {
         url : '/cats',
-        templateUrl: '/assets/views/cats.html',
-        controller: CatsCtrl
+        controller: CatsCtrl,
+        views: { 
+          'cats-view': { 
+            templateUrl: '/assets/views/cats.html',
+            controller: CatsCtrl,
+            resolve: {
+              cats : CatsCtrl.resolve
+            }
+          }
+        }
       })
       .state('cats.upload', {
         url: '^/upload',
-        templateUrl : '/assets/views/upload.html',
-        controller: UploadCtrl
-      })
-      .state('cats.cat', {
-        url: '/:id',
-        controller: function($stateParams, $rootScope) {
-          console.log("cats ctrl")
-          //$rootScope.selectedId = $stateParams.id;
+        views : {
+          'upload-view@' : {
+            templateUrl : '/assets/views/upload.html',
+            controller: UploadCtrl            
+          }
         }
       });
-  });
-
-  app.directive('scrollIf', function () {
+  })
+  .directive('scrollIf', function () {
     return function (scope, element, attributes) {
       setTimeout(function () {
         if (scope.$eval(attributes.scrollIf)) {
@@ -32,33 +35,37 @@ var app = angular.module('cats', ['ui.state', '$strap'])
         }
       });
     }
+  })
+  .value('$anchorScroll', angular.noop); // Disable scroll to top when changing ui states
+
+function CatsCtrl($scope, $http, cats) {
+  $scope.cats = cats.data;
+
+  $scope.selectOrder = [{
+    "text" : "Cats",
+    "click" : "sortByCat()"
+  }, {
+    "text" : "Bread Rage Abuse",
+    "click" : "sortByBread()"
+  }]
+
+  $scope.$watch('isSortByCat', function() {
+    $scope.sortingBy = $scope.isSortByCat ?  $scope.selectOrder[0].text : $scope.selectOrder[1].text;
+    $scope.direction = $scope.isSortByCat ? 1 : -1;
   });
 
-app.value('$anchorScroll', angular.noop); // Disable scroll to top when changing ui states
-
-function CatsCtrl($scope, $state, $stateParams, $http, $rootScope) {
-
-  console.log("CatsCtrl", $state.params);
-
   $scope.isSortByCat = true;
-  var direction = 1 ;
-  $scope.sortingBy = "Cats";
 
-  //$rootScope.selectedId = 0;
-
-  function scrollToCat(id) {
-    setTimeout(function () {
-      var element = $("a[href$='cats/" + id + "']").get();
-      //if (element.length > 0)  window.scrollTo(0, element[0].offsetTop - 100)
-    });
-  }
+  $scope.$on('cat-uploaded', function(e, cat) {
+    $scope.cats.push(cat);
+  });
 
   function updateCat(cat) {
     var copyOfCat = angular.copy(cat);
     delete copyOfCat.expandera;
 
     $http.put('/cats', copyOfCat)
-      .success(function(data, status) {  })
+      .success(function(data, status) {})
       .error(function(data, status) { alert(status) });
   }
 
@@ -66,72 +73,38 @@ function CatsCtrl($scope, $state, $stateParams, $http, $rootScope) {
     cat.score += direction;
 
     updateCat(cat);
-
-    //$state.transitionTo('cats.cat', {id: cat.id});
-    
-    //scrollToCat(cat.id);
   }
 
-  $rootScope.$watch('selectedId', function() {
-    //scrollToCat($rootScope.selectedId);
-  });
-
-  $scope.$on('refresh', function(e, data) {
-    $scope.cats = data;
-    $scope.selectedId = $scope.cats[$scope.cats.length - 1].id;
-  });
-  
   $scope.getScore = function(cat) {
-    return cat.score * direction;
+    return cat.score * $scope.direction;
   }
 
   $scope.voteUp = function(cat) {
-    vote(cat, direction);
+    vote(cat, $scope.direction);
   }
 
   $scope.voteDown = function(cat) {
-    vote(cat, -direction);
+    vote(cat, -$scope.direction);
   }
 
-  $http.get('/cats')
-    .success(function(data, status) {
-      $scope.cats = data;
-    })
-    .error(function(data, status) {
-      console.log("wtf");
-    });
-
-   $scope.toggle = function(cat) {
-      cat.expandera = cat.expandera ? false : true;
-    }
-
-  $scope.selectOrder = [
-  {
-    "text" : "Cats",
-    "click" : "sortByCat()"
-  },
-  {
-    "text" : "Bread Rage Abuse",
-    "click" : "sortByBread()"
-  }]
+  $scope.toggle = function(cat) {
+    cat.expandera = !cat.expandera;
+  }
 
   $scope.sortByCat = function() {
     $scope.isSortByCat = true;
-    direction = 1;
-    $scope.sortingBy = "Cats";
   }
 
   $scope.sortByBread = function() {
     $scope.isSortByCat = false;
-    direction = -1;
-    $scope.sortingBy = "Bread Rage Abuse";
   }
 }
 
+CatsCtrl.resolve = function($http) {
+  return $http.get('/cats');
+}
 
 function UploadCtrl($scope, $rootScope, $http, $state) {
-  console.log("Upload ctrl")
-
   $scope.cat = {};
 
   $scope.close = function() {
@@ -139,17 +112,13 @@ function UploadCtrl($scope, $rootScope, $http, $state) {
   }
 
   $scope.upload = function() {
-    console.log("Laddar upp: " + $scope.cat.name, $scope.cat);
-
     $http.post('/cats', $scope.cat)
       .success(function(data, status, headers) {
-        $scope.cats.push(data);
-        $state.transitionTo('cats.cat', {id:data.id});
+        $rootScope.$broadcast('cat-uploaded', data);
+        $state.transitionTo('cats');
       })
       .error(function(data, status) {
-        console.log("fan: " + status);
+        alert(status);
       });
   }
-
 }
-
