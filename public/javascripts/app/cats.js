d5661a68e87d06eb8a8a76b77245789fc80038f0
axelo <axel.olsson@gmail.com>
2013-06-09 23:34:59 +0200

    unz

--- public/javascripts/app/cats.js
+++ public/javascripts/app/cats.js
@@ -19,8 +19,58 @@ var app = angular.module('cats', ['ui.state', '$strap'])
       });
   });
 
+app.animation('myanim', function() {
+  return {
+    setup: function(el) {
+      /*var up = el[0];
+      var down = el[0].nextSibling;
+
+      var height = jQuery(up).outerHeight(true);
+
+      var diff = up.offsetTop - down.offsetTop;
+
+      console.log(diff);
+
+      up.style.top = height + "px";
+      down.style.top = -height + "px";*/
+
+      //console.log(height);
+
+      //jQuery(up).css({'top': height + "px"});
+      //jQuery(down).css({'top': -height + "px"});
+    },
+    start: function(el, done, memo) {
+      /*console.log("start");
+      var up = el[0];
+      var down = el[0].nextSibling;
+
+      var upDone = false;
+      var downDone = false;
+
+      jQuery(up).animate({
+        'top':0
+      }, function() {
+        upDone = true;
+        if (upDone && downDone) done(); 
+      });
+
+      jQuery(down).animate({
+        'top':0
+      }, function() {
+        downDone = true;
+        if (upDone && downDone) done(); 
+      });*/
+    },
+    cancel: function(el, done) {
+      console.log("cancel");
+    }
+  };
+});
+
 function CatsCtrl($scope, $http, cats) {
   $scope.cats = cats.data;
+  sortCats();
+
   $scope.isSortPopular = true;
 
   $scope.sortOrder = [{
@@ -39,9 +89,48 @@ function CatsCtrl($scope, $http, cats) {
     $scope.cats.push(cat);
   });
 
+  $scope.$watch('cats', function(newVal, oldVal, scope) {
+    sortCats();
+
+    /*console.log(newVal, oldVal);
+    console.log(scope.$index, scope);
+    sortCats();*/
+  }, true);
+
+  function sortCats() {
+    $scope.cats.sort(function(a, b) {
+      /*var compare = b.score - a.score;
+
+      if (compare === 0 && a.order && b.order) {
+        console.log(a.order);
+        console.log(b.order);
+        console.log(b.order - a.order);
+
+        var h = (b.order - a.order) + "";
+        console.log("h", h)
+        //return parseInt(h);
+        return 1;
+      }*/
+
+      /*if (compare === 0) {
+        //compare = b.order - a.order;v
+        var s = b.order;
+        var t = a.order;
+        compare = s - t;
+      }*/
+
+      return 1;
+    });
+
+    $scope.cats.forEach(function(cat, i) {
+      cat.order = i;
+    });
+  }
+
   function updateCat(cat) {
     var copyOfCat = angular.copy(cat);
     delete copyOfCat.expandera;
+    delete copyOfCat.order;
 
     $http.put('/cats', copyOfCat)
       .success(function(data, status) {})
@@ -68,6 +157,16 @@ function CatsCtrl($scope, $http, cats) {
 
   $scope.sortByMostPopular = function(val) {
     $scope.isSortPopular = val;
+  }
+
+  $scope.catComparator = function(cat) {
+    return cat.score;
+  }
+
+  $scope.setPos = function(cat) {
+    return {
+      top: cat.order * 208 + 'px'
+    };
   }
 }
 
---
+++ public/javascripts/cats.js
@@ -0,0 +1,103 @@
+var app = angular.module('cats', ['ui.state'])
+  .config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider
+      .otherwise('/cats');
+
+    $stateProvider
+      .state('cats', {
+        url : '/cats',
+        templateUrl: '/assets/views/cats.html',
+        controller: CatsCtrl
+      })
+      .state('cats.upload', {
+        url: '^/upload',
+        templateUrl : '/assets/views/upload.html',
+        controller: UploadCtrl
+      })
+      /*.state('cats.cat', {
+
+      })*/;
+  });
+
+app.directive('scrollIf', function () {
+  return function (scope, element, attributes) {
+    setTimeout(function () {
+      if (scope.$eval(attributes.scrollIf)) {
+        window.scrollTo(0, element[0].offsetTop - 100)
+      }
+    });
+  }
+});
+
+function CatsCtrl($scope, $state, $stateParams, $http) {
+  console.log("CatsCtrl", $stateParams);
+
+  $scope.$on('refresh', function(e, data) {
+    $scope.cats = data;
+    $scope.selectedId = $scope.cats[$scope.cats.length - 1].id;
+  });
+
+  $scope.isSortByCat = $stateParams.sortby ? $stateParams.sortby === 'cats' : true;
+  
+  var direction = $scope.isSortByCat ? 1 : -1;
+
+  $scope.setListHeight = function(list) {
+    console.log(list);
+    return {};
+    //return 100 + "px";
+  }
+
+  $scope.getScore = function(cat) {
+    return cat.score * direction;
+  }
+
+  $scope.voteUp = function(cat) {
+    cat.score += 1 * direction;
+    $scope.selectedId = cat.id;
+  }
+
+  $scope.voteDown = function(cat) {
+    cat.score -= 1 * direction;
+    $scope.selectedId = cat.id;
+  }
+
+  $http.get('/cats')
+    .success(function(data, status) {
+      $scope.cats = data;
+    })
+    .error(function(data, status) {
+      console.log("wtf");
+    });
+
+   $scope.toggle = function(cat) {
+      cat.expandera = cat.expandera ? false : true;
+    }
+}
+
+function UploadCtrl($scope, $http, $state) {
+  console.log("Upload ctrl")
+
+  $scope.cat = {};
+
+  $scope.upload = function() {
+    console.log("Laddar upp: " + $scope.cat.name, $scope.cat);
+
+    $http.post('/cats', $scope.cat)
+      .success(function(data, status) {
+
+        $http.get('/cats') // temp hack :P
+          .success(function(data, status) {
+            $scope.$emit('refresh', data);
+            $state.transitionTo('cats');
+          })
+          .error(function(data, status) {
+            console.log("wtf");
+          });
+      })
+      .error(function(data, status) {
+        console.log("fan: " + status);
+      });
+  }
+
+}
